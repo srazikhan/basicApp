9e04e3b638f04ba6b5aabd02ae463e08ccede592
nadeemsoft <nadeemashrafi38@gmail.com>
2022-05-25 09:23:10 +0530

    modified env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   apiUrl:"http://localhost:8080/api/",
   xyz:"http://localhost:8080/api/",
+  
   encryptSecretKey:"247whdskjnbakjaa2buewqikna!~@#keklkfdlmscmxnnz,m"
 };
 
